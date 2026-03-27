4930a2390d38559a0af4275c75b25872dd102a22
Harshini <hmanchana@gmail.com>
2026-03-27 13:50:56 +0000

    formatted tf files

--- main-project/main.tf
+++ main-project/main.tf
@@ -4,16 +4,16 @@ resource "aws_key_pair" "main" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source     = "./modules/vpc"
   cidr_block = var.cidr_block
 }
 
 module "subnet" {
-  source = "./modules/subnet"
-  vpc_id = module.vpc.vpc_id
+  source              = "./modules/subnet"
+  vpc_id              = module.vpc.vpc_id
   private_cidr_subnet = var.public_cidr_subnet
-  public_cidr_subnet = var.private_cidr_subnet
-  az = var.az
+  public_cidr_subnet  = var.private_cidr_subnet
+  az                  = var.az
 }
 
 module "igw" {
@@ -22,18 +22,18 @@ module "igw" {
 }
 
 module "Nat_Gateway" {
-  source = "./modules/Nat_Gateway"
-  igw_id = module.igw.gateway_id
+  source                = "./modules/Nat_Gateway"
+  igw_id                = module.igw.gateway_id
   public_cidr_subnet_id = module.subnet.public_cidr_subnet_id
 }
 
 module "Route_Table" {
-  source = "./modules/Route_Table"
-  vpc_id = module.vpc.vpc_id
-  nat_id = module.Nat_Gateway.nat_id
-  gateway_id = module.igw.gateway_id
+  source                 = "./modules/Route_Table"
+  vpc_id                 = module.vpc.vpc_id
+  nat_id                 = module.Nat_Gateway.nat_id
+  gateway_id             = module.igw.gateway_id
   private_cidr_subnet_id = module.subnet.private_cidr_subnet_id
-  public_cidr_subnet_id = module.subnet.public_cidr_subnet_id
+  public_cidr_subnet_id  = module.subnet.public_cidr_subnet_id
   destination_cidr_block = var.destination_cidr_block
 }
 
@@ -43,23 +43,23 @@ module "security_group" {
 }
 
 module "ALB" {
-  source = "./modules/ALB"
-  vpc_id = module.vpc.vpc_id
-  alb_sg_id = module.security_group.alb_sg_id
+  source                = "./modules/ALB"
+  vpc_id                = module.vpc.vpc_id
+  alb_sg_id             = module.security_group.alb_sg_id
   public_cidr_subnet_id = module.subnet.public_cidr_subnet_id
 }
 
 module "Bastion_Ec2" {
-  source = "./modules/Bastion_Ec2"
+  source                = "./modules/Bastion_Ec2"
   public_cidr_subnet_id = module.subnet.public_cidr_subnet_id[0]
-  bastion_sg_id = module.security_group.bastion_sg_id
-  key_name = aws_key_pair.main.key_name
+  bastion_sg_id         = module.security_group.bastion_sg_id
+  key_name              = aws_key_pair.main.key_name
 }
 
 module "Ec2" {
-  source = "./modules/Ec2"
-  ec2_sg_id = module.security_group.ec2_sg_id
+  source                 = "./modules/Ec2"
+  ec2_sg_id              = module.security_group.ec2_sg_id
   private_cidr_subnet_id = module.subnet.private_cidr_subnet_id
-  alb_tg_arn = module.ALB.alb_tg_arn
-  key_name = aws_key_pair.main.key_name
+  alb_tg_arn             = module.ALB.alb_tg_arn
+  key_name               = aws_key_pair.main.key_name
 }
